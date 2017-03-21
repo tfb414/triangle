8a7e7929a4eeceb7d6925260950765c3833eaf69
Tim Brady <tfb414@gmail.com>
2017-03-21 10:09:54 -0400

    minor fix

--- triangle.js
+++ triangle.js
@@ -6,17 +6,17 @@ function triangleTest(side1, side2, side3){
 function invalidTriangleTester(side1, side2, side3){
   for (var i in arguments){
     if(Math.sign(arguments[i]<=0)){
-      throw "Error: each side of the triangle must be a positive number";
+        throw "Error: each side of the triangle must be a positive number";
     }
   }
     function impossibleTriangle() {
         return side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2;
     }
     if (arguments.length !== 3){
-    throw "Error: a triangle must have 3 sides";
+        throw "Error: a triangle must have 3 sides";
   }
-  else if(impossibleTriangle()){
-      throw "Error: the sum of two sides must be greater than the third";
+    else if(impossibleTriangle()){
+        throw "Error: the sum of two sides must be greater than the third";
   }
 }
 
@@ -31,14 +31,14 @@ function typeOfTriange(side1, side2, side3){
     }
 
     if (allSameLengths()){
-      return "equilateral"
+        return "equilateral"
     }
     else if(twoSameLengths()){
-      return "isosceles";
+        return "isosceles";
     }
     else{
-      return "scalene"
+        return "scalene"
     }
   }
 
-triangleTest(1,2); 
+triangleTest(1,2);
