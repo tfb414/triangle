98cd51eb9d0adee67a5f670aa912c1a9ce17c8b5
Tim Brady <tfb414@gmail.com>
2017-03-21 12:54:55 -0400

    another refactoring

--- triangle.js
+++ triangle.js
@@ -1,35 +1,37 @@
 function triangleTest(side1, side2, side3){
   invalidTriangleTester.apply(this, arguments);
-  invalidTriangleTester(side1, side2, side3);
-  return typeOfTriange(side1, side2, side3);
+  return typeOfTriange.apply(this, arguments);
 }
 function invalidTriangleTester(side1, side2, side3){
-  for (var i in arguments){
-    if(Math.sign(arguments[i]<=0)){
-        throw "Error: each side of the triangle must be a positive number";
+    function hasNegativeLength() {
+        return arguments[i] <= 0;
     }
-  }
     function impossibleTriangle() {
         return side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2;
     }
-    if (arguments.length !== 3){
+    function impossibleNumberOfSides() {
+        return arguments.length !== 3;
+    }
+    for (var i in arguments){
+        if(hasNegativeLength.apply(this,arguments)){
+            throw "Error: each side of the triangle must be a positive number";
+        }
+    }
+    if (impossibleNumberOfSides.apply(this, arguments)){
         throw "Error: a triangle must have 3 sides";
-  }
+    }
     else if(impossibleTriangle()){
         throw "Error: the sum of two sides must be greater than the third";
-  }
+    }
 }
 
 function typeOfTriange(side1, side2, side3){
-
     function allSameLengths() {
         return side1 === side2 && side2 === side3;
     }
-
     function twoSameLengths() {
         return side1 === side2 || side2 === side3 || side1 === side3;
     }
-
     if (allSameLengths()){
         return "equilateral"
     }
@@ -41,4 +43,4 @@ function typeOfTriange(side1, side2, side3){
     }
   }
 
-triangleTest(1,2);
+triangleTest(1,2,2);
